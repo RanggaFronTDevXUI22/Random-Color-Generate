d0931e30f2c4a6a3d78767d5f3345f8dfa657071
RanggaSiFrontEnd <ranggapratamaramadhan08@gmail.com>
2026-06-17 09:56:18 +0700

    Delete Password Generator directory

--- Password Generator/app.py
+++
@@ -1,9 +0,0 @@
-print("🔍 WEBSITE URL CHECKER 🔍")
-url = input("\nEnter a website URL: ")
-
-if url.startswith("https://"):
-    print("🔐 This website uses HTTPS (secure)")
-elif url.startswith("http://"):
-    print("👀 This website uses HTTP (not secure)")
-else:
-    print("❌ This doesn't look like a complete URL")
